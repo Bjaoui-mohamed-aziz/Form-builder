11548b6b1dfaadf5cbe61bd452c7544864085bfe
Bjaoui-mohamed-aziz <azizbjaoui312@gmail.com>
2024-08-07 13:12:30 +0200

    added and designed the eye icon to preview modal

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import eye from 'assets/icons/eye.svg';  
 type FormComponentProps = {
   
   id: string;
@@ -17,14 +17,20 @@ const FormComponent: React.FC<FormComponentProps> = ({id, name, type, onDelete }
       
   return (
     <div className="w-60 h-48  flex flex-col m-4 border-2 border-gray-300 p-4 mb-4 shadow-lg rounded-lg bg-white relative hover:bg-gray-100 hover:shadow-2xl transition duration-300 hover:scale-105">
+             <div className='static'>
+              <img
+              src={eye}
+              alt="Text Input"
+              className="absolute top-2 right-2 w-10 h-10 hover:bg-gray-300 rounded-md p-1" 
+            /></div>
       <h3 className="text-xl mb-2 text-[#233368] font-semibold">{name}</h3>
       <p className="text-[#3b549f]">Type: {type}</p>
       <div className="flex absolute bottom-3 left-3">
-        <button className="p-2 bg-gray-300 mr-2 text-black rounded" onClick={() => onDelete(id)}>
+        <button className="p-2 bg-gray-300 mr-2 text-black rounded hover:bg-gray-400" onClick={() => onDelete(id)}>
           Delete
         </button>
         <Link to={`/form-builder/${id}`}>
-          <button className="p-2 bg-[#233368] text-white rounded">
+          <button className="p-2 bg-[#233368] text-white rounded hover:bg-[#2f3e6f]">
             Edit
           </button>
         </Link>
